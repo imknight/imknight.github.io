db05456bd2d1e3236f250ccafae64fa03e9d9f6c
Knight Tan <knight@reworklabs.com>
2025-05-28 06:32:35 +0800

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,5 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-  }),
-});
-
 const logs = defineCollection({
   type: "content",
   schema: z.object({
@@ -30,27 +20,5 @@ const products = defineCollection({
   }),
 });
 
-const work = defineCollection({
-  type: "content",
-  schema: z.object({
-    company: z.string(),
-    role: z.string(),
-    dateStart: z.coerce.date(),
-    dateEnd: z.union([z.coerce.date(), z.string()]),
-  }),
-});
-
-const projects = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-    demoURL: z.string().optional(),
-    repoURL: z.string().optional(),
-  }),
-});
-
 //export const collections = { blog, work, projects };
-export const collections = { logs, products, work, projects, blog };
+export const collections = { logs, products };
